8412fd14343cb9122687e984e16052d060bdec10
Ilia Deriglazov <m313am@gmail.com>
2022-09-11 16:50:14 -0400

    Added positive tests for input fields

--- cypress/E2E/accessTravel.cy.js
+++ cypress/E2E/accessTravel.cy.js
@@ -1,10 +1,32 @@
 ///<reference types = "Cypress"/>
 describe('Accesstravel tests', () =>{
 
-    it('Navigate to the website',() => {
+    it('Accesstravel.com tests',() => {
         cy.visit('https://www.accesstravel.com/en-US');
         cy.get('.attractions-search__head-title',{timeout: 10000});
+        //The line below is commented out because it does not work
         //cy.get('.attractions-search__head-title').contains('INCLUSIVE ATTRACTIONS AND TOURS').invoke('text').should('equal', 'INCLUSIVE ATTRACTIONS AND TOURS');
         cy.url().should('eq', 'https://www.accesstravel.com/en-US');
+        //The assignment instructions say to press the Hotels button, but I had to navigate to the home page first 
+        //as the button is not present on the https://www.accesstravel.com/en-US page
+        cy.visit('https://www.accesstravel.com/en-US/Home/Index');
+        cy.get('[class="hotels"]',{timeout: 10000}).click();
+        
+        cy.get('#Filter_DestinationId').select('Paris');
+        cy.get('#Filter_DestinationId').invoke('val').should('equal', '4');
+
+        cy.get('[name="Filter.CheckIn"]',{timeout: 10000}).click();
+        cy.get(':nth-child(5) > :nth-child(6) > .ui-state-default').click();
+        cy.get('[name="Filter.CheckIn"]').invoke('val').should('equal', '2022-09-30');
+
+        cy.get('[name="Filter.CheckOut"]').clear().type('2022-10-20');
+        cy.get('[name="Filter.CheckOut"]').invoke('val').should('equal', '2022-10-20');
+
+        cy.get('[name="Filter.AdultNum"]').clear().type('1');
+        cy.get('[name="Filter.AdultNum"]').invoke('val').should('equal', '1');
+        
+        cy.get('[name="Filter.ChildrenNum"]').clear().type('1');
+        cy.get('[name="Filter.ChildrenNum"]').invoke('val').should('equal', '1');
+         
     });
 });
